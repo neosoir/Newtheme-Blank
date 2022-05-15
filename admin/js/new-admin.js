1ecde23f7a3bc185206053c0a27fdf561390b611
Neo Soir <leonard@neosoir.com>
2022-05-16 01:19:22 +0200

    [add] create user button logic

--- admin/js/new-admin.js
+++ admin/js/new-admin.js
@@ -13,4 +13,35 @@
 
 	});
 
+	// Create table.
+	$(document).ready( function() {
+
+		$('#crear-tabla').on('click', function(e) {
+
+			e.preventDefault();
+
+			var nombre = $('#nombre-tabla');
+			var n = nombre.val();
+			var mensaje = $('#add_new_table #mensaje');
+
+			if ( n != '' ) {
+				
+				// Envio ajax.
+				mensaje.html('');
+
+			}
+			else {
+
+				if ( !nombre.hasClass('invalid') ) {
+					
+					nombre.addClass('invalid');
+					nombre.after('<p id="mensaje">Insertar nombre de la tabla.</p>')
+
+				}
+
+			}
+		});
+
+	});
+
 })( jQuery );
